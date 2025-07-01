6fe99d9261030607b1fe1eb83f70159409b34d6e
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-07-01 11:20:00 +0200

    test ajput code localestorage dans sw.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -9,3 +9,96 @@ self.addEventListener("activate", function (event) {
   console.log("Service Worker activé");
   self.clients.claim(); // Prendre le contrôle des pages ouvertes
 });
+
+const staticCacheName = "my-gaming-site-cache-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./offline.html",
+  "./random.html",
+  "./open.html",
+  "./share.html",
+  "./style.css",
+  "./app.js",
+  "./serviceWorker.js",
+  "./manifest.json",
+  "./assets/manifest-icon-192.maskable.png",
+  "./assets/manifest-icon-512.maskable.png",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  const cacheWhitelist = [staticCacheName];
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((k) => !cacheWhitelist.includes(k))
+          .map((k) => caches.delete(k))
+      );
+    })
+  );
+});
+
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       if (response) {
+//         return response;
+//       }
+
+//       return fetch(event.request)
+//         .then(function (networkResponse) {
+//           if (
+//             !networkResponse ||
+//             networkResponse.status !== 200 ||
+//             networkResponse.type !== "basic"
+//           ) {
+//             return networkResponse;
+//           }
+
+//           const responseToCache = networkResponse.clone();
+//           caches.open(staticCacheName).then(function (cache) {
+//             cache.put(event.request, responseToCache);
+//           });
+
+//           return networkResponse;
+//         })
+//         .catch(function (error) {
+//           console.error("Fetch failed; returning offline fallback:", error);
+//           if (event.request.mode === "navigate") {
+//             return caches.match("./offline.html");
+//           }
+
+//           return new Response(
+//             "Vous êtes hors ligne et la ressource n'est pas en cache.",
+//             {
+//               status: 503,
+//               statusText: "Service Unavailable",
+//               headers: new Headers({ "Content-Type": "text/plain" }),
+//             }
+//           );
+//         });
+//     })
+//   );
+// });
+
+// Intercepter les requêtes pour servir depuis le cache
+self.addEventListener("fetch", (event) => {
+  console.log("🛰 Fetch:", event.request.url);
+
+  event.respondWith(
+    // indice: permet de renvoyer une réponse custom
+    caches
+      .match(event.request) // cherche dans le cache
+      .then((res) => res || fetch(event.request)) // si pas trouvé, va le chercher en ligne
+  );
+});
