3f2c4657b32f5fc41c026f04b4e8cd8e39c0dbcb
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-07-01 09:50:48 +0200

    feat : serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,12 @@
 var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = ["/", "/styles/main.[hash].css", "/script/main.[hash].js"];
 
+// <!-- Écouter l'installation du SW -->
 self.addEventListener("install", function (event) {
+  // indice: quand le SW est installé
+  console.log(" Service Worker installé");
+  self.skipWaiting(); // indice: forcer à prendre le contrôle immédiatement
+
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -11,8 +16,13 @@ self.addEventListener("install", function (event) {
   );
 });
 
+// <!-- Écouter l'activation du SW -->
 self.addEventListener("activate", function (event) {
   var cacheWhitelist = [CACHE_NAME];
+
+  // indice: quand le SW devient actif
+  console.log(" Service Worker activé");
+  self.clients.claim(); // indice: prendre le contrôle des pages ouvertes
 
   event.waitUntil(
     // Check de toutes les clés de cache.
