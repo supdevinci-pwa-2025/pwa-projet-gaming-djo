2eddf9ab1959df750e15ea2422270500dfb85bd2
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-07-01 09:11:15 +0200

    feat : gestion du cache avec le serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = "my-site-cache-v1";
+var urlsToCache = ["/", "/styles/main.[hash].css", "/script/main.[hash].js"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    // Check de toutes les clés de cache.
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
